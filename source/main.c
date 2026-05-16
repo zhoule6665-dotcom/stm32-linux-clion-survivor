c93704869c59903a5218ba2217d1d12c54c9b036
dr_zhou <zhoule6665@gmail.com>
2026-05-16 15:13:12 +0800

    Add files via upload

---
+++ source/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stm32f10x_usart.h>
+#include "main.h"
+int main(void) {
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+    //GPIO_SetBits(GPIOC, GPIO_Pin_13);
+    GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+}
